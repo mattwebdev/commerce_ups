15608bab12ca37b6f976e5bd5cb4f569762ed093
mattwebdev <matt.heinke@gmail.com>
2017-08-02 13:38:12 -0700

    made package abit more clear, and added the physical methods.

--- src/Services/UPSPackage.php
+++ src/Services/UPSPackage.php
@@ -1,13 +1,13 @@
 <?php
 namespace Drupal\commerce_ups\Services;
 
-use Ups\Entity\Dimensions;
+use Ups\Entity\Dimensions as PackageDimensions;
 use Ups\Entity\Package;
 use Ups\Entity\PackageWeight;
 use Ups\Entity\PackagingType;
 
 class UPSPackage {
-  public function __construct(PackagingType $packageType, Dimensions $dimensions, PackageWeight $weight) {
+  public function __construct(PackagingType $packageType, PackageDimensions $dimensions, PackageWeight $weight) {
     $package = new Package();
     $package->setPackagingType($packageType);
     $package->setDimensions($dimensions);
--- src/Services/UPSPhysical.php
+++ src/Services/UPSPhysical.php
@@ -1,8 +1,43 @@
 <?php
 namespace Drupal\commerce_ups\Services;
 
-class UPSPhysical {
-  public function __construct() {
+use Drupal\physical\Dimensions;
+use Drupal\physical\Weight;
+use Ups\Entity\Dimensions as PackageDimensions;
+use Ups\Entity\PackageWeight;
+use Ups\Entity\UnitOfMeasurement;
 
+class UPSPhysical {
+  public function getWeight(Weight $weight) {
+    $UPSWeight = new PackageWeight();
+    $UPSWeight->setUnitOfMeasurement($this->translateWeightUnits($weight));
+    $UPSWeight->setWeight($weight->getNumber());
+
+    return $UPSWeight;
+
+  }
+
+  public function getDimensions(Dimensions $dimensions) {
+    $UPSDimensions = new PackageDimensions();
+    $UPSDimensions->setUnitOfMeasurement($this->translateUnitOfMeasurement($dimensions));
+    $UPSDimensions->setHeight($dimensions->getHeight());
+    $UPSDimensions->setLength($dimensions->getLength());
+    $UPSDimensions->setWidth($dimensions->getWidth());
+
+    return $UPSDimensions;
+  }
+
+  public function translateUnitOfMeasurement(Dimensions $dimensions) {
+    $UPSUnit = new UnitOfMeasurement();
+    $Unit = $dimensions->getUnit();
+    $UPSUnit->setCode($Unit);
+    return $UPSUnit;
+  }
+
+  public function translateWeightUnits(Weight $weight) {
+    $UPSWeightUnit = new UnitOfMeasurement();
+    $weightUnit = $weight->getUnit();
+    $UPSWeightUnit->setCode($weightUnit);
+    return $UPSWeightUnit;
   }
 }
