6ac127bf6ef59f31e7387edc12215355bfcad58e
mattwebdev <matt.heinke@gmail.com>
2017-08-11 11:45:02 -0700

    derp, gotta submit it

--- src/Plugin/Commerce/ShippingMethod/CommerceUps.php
+++ src/Plugin/Commerce/ShippingMethod/CommerceUps.php
@@ -168,6 +168,8 @@ class CommerceUps extends ShippingMethodBase {
         $this->configuration['api_information']['password'] = $values['api_information']['password'];
       }
       $this->configuration['api_information']['mode'] = $values['api_information']['mode'];
+      $this->configuration['api_information']['nRate'] = $values['api_information']['nRate'];
+
 
       $this->configuration['options']['packaging'] = $values['options']['packaging'];
       $this->configuration['options']['log'] = $values['options']['log'];
